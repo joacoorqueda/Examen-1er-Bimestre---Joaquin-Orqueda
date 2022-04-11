aca815159804ce2db0d1b8769d264fbfe901b16b
joacoorqueda <101809787+joacoorqueda@users.noreply.github.com>
2022-04-11 13:39:05 -0300

    consigna copiada

--- Assets/Scripts/Ej1.cs
+++ Assets/Scripts/Ej1.cs
@@ -1,6 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Todos los ingresos se hacen por Inspector.
+//Ingresar:
+//nombre de alumno
+//de qué año es(un número del 1 al 5)
+//a qué orientación pertenece(sólo se puede ingresar la inicial en mayúscula de la especialidad:
+//    T para TIC, D para Diseño, G para Gestión, M para Medios y H para Humanidades).
+//El programa debe cumplir los siguientes requerimientos:
+//Mostrar un mensaje de error si el nombre ingresado es una cadena de caracteres vacía y
+//    si el año ingresado no está en el rango del 1 al 5. 
+//Si se ingresa un valor diferente a las opciones válidas en el campo ingreso de orientación 
+//    escribir en consola “Solo puede ingresar T, D, G, M o H”. 
+
+//Si el año ingresado no está entre 3 y 5 se debe mostrar el mensaje “Error.Aún estás en el ciclo básico”.
+
+//Si los ingresos de nombre, año y orientación son válidos escribir en consola un mensaje de
+//    agradecimiento en el siguiente formato: “Muchas gracias[nombreAlumno]!”
 
 public class Ej1 : MonoBehaviour
 {
